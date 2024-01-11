bda312bfec3c37a51d4e845b20dfa665c5ea0400
prasetyatf <tegarfitriaji@gmail.com>
2024-01-11 13:28:14 +0700

    project 6(the room furniture landing page)

---
+++ room-homepage-master/README.md
@@ -0,0 +1,41 @@
+# Room homepage solution
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+**Note: Delete this note and update the table of contents based on what sections you keep.**
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Navigate the slider using either their mouse/trackpad or keyboard
+
+### Screenshot
+
+![](./screenshot.jpg)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- [Styled Components](https://styled-components.com/) - For styles
+
+## Author
+
+- Website - [prasetyatf](https://www.linkedin.com/in/prasetyatf)
---
+++ room-homepage-master/js/script.js
@@ -0,0 +1,57 @@
+// manipulate classlist untuk tombol hamburgermenu dan close button
+const hamburgerMenu = document.getElementById("hamburger-menu");
+const subMenu = document.querySelector("ul.sub-menu");
+const closeButton = document.getElementById("close");
+
+hamburgerMenu.addEventListener("click", function () {
+  subMenu.classList.add("active");
+});
+
+closeButton.addEventListener("click", function () {
+  subMenu.classList.remove("active");
+});
+
+// buat slider yang mengubah background img
+const header = document.querySelector("header");
+const backButton = document.querySelector(".navigate a#back-button");
+const nextButton = document.querySelector(".navigate a#next-button");
+
+const bgImgArray = [
+  "url('/images/desktop-image-hero-1.jpg')",
+  "url('/images/desktop-image-hero-2.jpg')",
+  "url('/images/desktop-image-hero-3.jpg')",
+];
+
+
+let currentIndex = 0;
+const maxIndex = bgImgArray.length - 1;
+const minIndex = bgImgArray.length - bgImgArray.length;
+
+backButton.addEventListener("click", function () {
+  // currentIndex = (currentIndex - 1 + bgImgArray.length) % bgImgArray.length;
+  currentIndex--;
+  if(currentIndex < minIndex){
+    currentIndex = maxIndex;
+    header.style.backgroundImage = bgImgArray[currentIndex];
+    console.log(header.style.backgroundImage = bgImgArray[currentIndex]);
+  }
+  else{
+    header.style.backgroundImage = bgImgArray[currentIndex];
+    console.log(header.style.backgroundImage = bgImgArray[currentIndex]);
+  }
+
+});
+
+nextButton.addEventListener("click", function () {
+  currentIndex++;
+  if(currentIndex > maxIndex){
+    currentIndex = minIndex;
+    header.style.backgroundImage = bgImgArray[currentIndex];
+    console.log(header.style.backgroundImage = bgImgArray[currentIndex]);
+  }
+  else{
+    header.style.backgroundImage = bgImgArray[currentIndex];
+    console.log(header.style.backgroundImage = bgImgArray[currentIndex]);
+  }
+
+});
